151e4341c30d4899c4de8a4122b9ba12aaad2836
Xorse Mawulorm Ablorde <ablordefaith@gmail.com>
2024-11-06 18:20:00 +0000

    Made changes to our view

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask Python!"
+    return "Hello, Flask Python! We are building an automatic deployment to AWS using codepipeline"
 
 
 if __name__ == '__main__':
